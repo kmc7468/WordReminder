157fb08349fecc55de7b1ce0ffb325cb79b24706
static <kmc7468@naver.com>
2020-11-01 01:26:12 +0900

    QuestionOptionWindow의 체크 박스의 가로 크기를 조금 줄임

--- QuestionOptionWindow.c
+++ QuestionOptionWindow.c
@@ -21,9 +21,9 @@ LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wPar
 			10, 35, WIDTH - 35, 50, handle, 0);
 
 		g_GuessingMeaningButton = CreateAndShowChild(_T("button"), _T("단어 보고 뜻 맞추기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
-			10, 130, 200, 30, handle, 1);
+			10, 130, 150, 30, handle, 1);
 		g_GuessingWordButton = CreateAndShowChild(_T("button"), _T("뜻 보고 단어 맞추기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
-			180, 130, 200, 30, handle, 2);
+			180, 130, 150, 30, handle, 2);
 
 		g_ShouldGivePronunciationButton = CreateAndShowChild(_T("button"), _T("발음 표시하기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
 			10, 195, 200, 30, handle, 3);
