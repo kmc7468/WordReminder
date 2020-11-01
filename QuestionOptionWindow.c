a8490b80303e8ff122d4e1ac8f11f70ad81309b0
static <kmc7468@naver.com>
2020-11-01 10:00:15 +0900

    변수 네이밍 룰 통일

--- QuestionOptionWindow.c
+++ QuestionOptionWindow.c
@@ -4,8 +4,8 @@
 
 #include <stdlib.h>
 
-static HWND g_SelectingVocabularyButton;
-static HWND g_GuessingMeaningButton, g_GuessingWordButton;
+static HWND g_SelectVocabularyButton;
+static HWND g_GuessMeaningButton, g_GuessWordButton;
 static HWND g_ShouldGivePronunciationButton;
 static HWND g_StartButton;
 
@@ -17,12 +17,12 @@ static bool g_ShouldEnableMainWindow = true;
 LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
 	EVENT {
 	case WM_CREATE:
-		g_SelectingVocabularyButton = CreateAndShowChild(_T("button"), _T("단어장 선택"), GlobalBoldFont, BS_PUSHBUTTON,
+		g_SelectVocabularyButton = CreateAndShowChild(_T("button"), _T("단어장 선택"), GlobalBoldFont, BS_PUSHBUTTON,
 			10, 35, WIDTH - 35, 50, handle, 0);
 
-		g_GuessingMeaningButton = CreateAndShowChild(_T("button"), _T("단어 보고 뜻 맞추기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
+		g_GuessMeaningButton = CreateAndShowChild(_T("button"), _T("단어 보고 뜻 맞추기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
 			10, 130, 150, 30, handle, 1);
-		g_GuessingWordButton = CreateAndShowChild(_T("button"), _T("뜻 보고 단어 맞추기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
+		g_GuessWordButton = CreateAndShowChild(_T("button"), _T("뜻 보고 단어 맞추기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
 			180, 130, 150, 30, handle, 2);
 
 		g_ShouldGivePronunciationButton = CreateAndShowChild(_T("button"), _T("발음 표시하기"), GlobalDefaultFont, BS_AUTOCHECKBOX,
@@ -44,7 +44,7 @@ LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wPar
 		return 0;
 
 	case WM_SIZE:
-		SetWindowPos(g_SelectingVocabularyButton, HWND_TOP, 0, 0, WIDTH - 35, 50, SWP_NOMOVE);
+		SetWindowPos(g_SelectVocabularyButton, HWND_TOP, 0, 0, WIDTH - 35, 50, SWP_NOMOVE);
 		SetWindowPos(g_StartButton, HWND_TOP, 10, HEIGHT - 100, WIDTH - 35, 50, 0);
 		return 0;
 
@@ -62,7 +62,7 @@ LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wPar
 
 	case WM_CTLCOLORSTATIC: {
 		const HWND btnHandle = (HWND)lParam;
-		if (btnHandle == g_GuessingMeaningButton || btnHandle == g_GuessingWordButton || btnHandle == g_ShouldGivePronunciationButton) {
+		if (btnHandle == g_GuessMeaningButton || btnHandle == g_GuessWordButton || btnHandle == g_ShouldGivePronunciationButton) {
 			SetBkMode((HDC)wParam, TRANSPARENT);
 		}
 		return 0;
@@ -73,7 +73,7 @@ LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wPar
 		case 0: {
 			const LPCTSTR path = ShowOpenFileDialog(handle);
 			if (path) {
-				SetWindowText(g_SelectingVocabularyButton, _T("단어장 선택됨"));
+				SetWindowText(g_SelectVocabularyButton, _T("단어장 선택됨"));
 				g_VocabularyPath = path;
 
 				if (g_Vocabularary) {
@@ -91,8 +91,8 @@ LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wPar
 				break;
 			}
 
-			const bool guessingMeaning = (bool)SendMessage(g_GuessingMeaningButton, BM_GETCHECK, 0, 0);
-			const bool guessingWord = (bool)SendMessage(g_GuessingWordButton, BM_GETCHECK, 0, 0);
+			const bool guessingMeaning = (bool)SendMessage(g_GuessMeaningButton, BM_GETCHECK, 0, 0);
+			const bool guessingWord = (bool)SendMessage(g_GuessWordButton, BM_GETCHECK, 0, 0);
 			if (!guessingMeaning && !guessingWord) {
 				MessageBox(handle, _T("문제 유형을 선택해 주세요."), _T("오류"), MB_OK | MB_ICONERROR);
 				break;
@@ -130,7 +130,7 @@ LRESULT CALLBACK QuestionOptionWindowProc(HWND handle, UINT message, WPARAM wPar
 		for (int i = 0; i < g_Vocabularary->Count; ++i) {
 			g_Vocabularary->Array[i].IsWrong = false;
 		}
-		SetWindowText(g_SelectingVocabularyButton, _T("단어장 선택됨"));
+		SetWindowText(g_SelectVocabularyButton, _T("단어장 선택됨"));
 		break;
 
 	case WM_GETMINMAXINFO: {
