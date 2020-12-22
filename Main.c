ad11726e217072063dc0899065044e74a769c169
static <kmc7468@naver.com>
2020-12-22 14:22:23 +0900

    MainWindow의 타이틀에 현재 버전이 표시되도록 개선

--- Main.c
+++ Main.c
@@ -6,6 +6,7 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #include <Windows.h>
 
 #include "Window.h"
+#include "Version.h"
 
 int APIENTRY WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdArgs, int cmdShow) {
 	if (!Initialize(instance)) {
@@ -13,7 +14,7 @@ int APIENTRY WinMain(HINSTANCE instance, HINSTANCE prevInstance, LPSTR cmdArgs, 
 		MessageBox(NULL, _T("프로그램을 초기화하는 중 오류가 발생했습니다."), _T("오류"), MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	MainWindow = CreateAndShowWindow(_T("MainWindow"), _T("단어 암기 프로그램"), cmdShow);
+	MainWindow = CreateAndShowWindow(_T("MainWindow"), _T("단어 암기 프로그램 ") WR_APPLICATION_VERSION, cmdShow);
 
 	MSG message;
 	while (GetMessage(&message, NULL, 0, 0)) {
