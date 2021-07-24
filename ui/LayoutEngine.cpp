0271cb2b707bedfe9e1c82530172a8f39c20bbe9
static <kmc7468@naver.com>
2021-07-24 22:17:20 +0900

    Division 클래스 추가

--- ui/LayoutEngine.cpp
+++ ui/LayoutEngine.cpp
@@ -20,11 +20,17 @@ double Length::GetEvaluated() const noexcept {
 }
 
 Length Length::Pixel(double value, ScaleMode scaleMode) {
+	assert(value > 0);
+	assert(scaleMode == DisplayBased || scaleMode == WindowBased);
+
 	return Length([value, scaleMode](const LengthEvaluationContext& evaluationContext) {
 		return value * (scaleMode == DisplayBased ? evaluationContext.DisplayScale : evaluationContext.WindowScale);
 	});
 }
 Length Length::Percent(double value) {
+	assert(value > 0);
+	assert(value <= 100);
+
 	return Length([value](const LengthEvaluationContext& evaluationContext) {
 		return evaluationContext.ParentLength * value / 100;
 	});
@@ -33,4 +39,88 @@ Length Length::WrapContent() {
 	return Length([](const LengthEvaluationContext&) {
 		return -1;
 	});
+}
+
+Division::Division(std::wstring name, Length width, Length height) noexcept
+	: m_Name(std::move(name)), m_Width(std::move(width)), m_Height(std::move(height)) {
+	assert(!m_Name.empty());
+}
+Division::Division(Component* component, Length width, Length height) noexcept
+	: m_Component(component), m_Width(std::move(width)), m_Height(std::move(height)) {
+	assert(component != nullptr);
+}
+Division::Division(std::wstring name, Component* component, Length width, Length height) noexcept
+	: m_Name(std::move(name)), m_Component(component), m_Width(std::move(width)), m_Height(std::move(height)) {
+	assert(!m_Name.empty());
+	assert(component != nullptr);
+}
+
+void Division::AddChild(Division&& child) {
+	m_Children.push_back(std::move(child));
+}
+
+Division& Division::SetMargin(const Length& margin) {
+	m_LeftMargin = m_RightMargin = m_TopMargin = m_BottomMargin = margin;
+	return *this;
+}
+Division& Division::SetHorizontalMargin(const Length& horizontalMargin) {
+	m_LeftMargin = m_RightMargin = horizontalMargin;
+	return *this;
+}
+Division& Division::SetVerticalMargin(const Length& verticalMargin) {
+	m_TopMargin = m_BottomMargin = verticalMargin;
+	return *this;
+}
+Division& Division::SetLeftMargin(Length leftMargin) noexcept {
+	m_LeftMargin = std::move(leftMargin);
+	return *this;
+}
+Division& Division::SetRightMargin(Length rightMargin) noexcept {
+	m_RightMargin = std::move(rightMargin);
+	return *this;
+}
+Division& Division::SetTopMargin(Length topMargin) noexcept {
+	m_TopMargin = std::move(topMargin);
+	return *this;
+}
+Division& Division::SetBottomMargin(Length bottomMargin) noexcept {
+	m_BottomMargin = std::move(bottomMargin);
+	return *this;
+}
+Division& Division::SetPadding(const Length& padding) {
+	m_LeftPadding = m_RightPadding = m_TopPadding = m_BottomPadding = padding;
+	return *this;
+}
+Division& Division::SetHorizontalPadding(const Length& horizontalPadding) {
+	m_LeftPadding = m_RightPadding = horizontalPadding;
+	return *this;
+}
+Division& Division::SetVerticalPadding(const Length& verticalPadding) {
+	m_TopPadding = m_BottomPadding = verticalPadding;
+	return *this;
+}
+Division& Division::SetLeftPadding(Length leftPadding) noexcept {
+	m_LeftPadding = std::move(leftPadding);
+	return *this;
+}
+Division& Division::SetRightPadding(Length rightPadding) noexcept {
+	m_RightPadding = std::move(rightPadding);
+	return *this;
+}
+Division& Division::SetTopPadding(Length topPadding) noexcept {
+	m_TopPadding = std::move(topPadding);
+	return *this;
+}
+Division& Division::SetBottomPadding(Length bottomPadding) noexcept {
+	m_BottomPadding = std::move(bottomPadding);
+	return *this;
+}
+
+Division& Division::SetHorizontalAlignment(Alignment horizontalAlignment) noexcept {
+	m_HorizontalAlignment = horizontalAlignment;
+	return *this;
+}
+Division& Division::SetVerticalAlignment(Alignment verticalAlignment) noexcept {
+	m_VerticalAlignment = verticalAlignment;
+	return *this;
 }
--- ui/LayoutEngine.hpp
+++ ui/LayoutEngine.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "../pal/Component.hpp"
+
 #include <functional>
+#include <optional>
+#include <string>
+#include <vector>
 
 struct LengthEvaluationContext final {
 	double DisplayScale;
@@ -38,4 +43,64 @@ public:
 	static Length Pixel(double value, ScaleMode scaleMode = DisplayBased);
 	static Length Percent(double value);
 	static Length WrapContent();
+};
+
+class LayoutEngine;
+
+class Division final {
+	friend class LayoutEngine;
+
+public:
+	enum Alignment {
+		Left,
+		Top = Left,
+		Center,
+		Right,
+		Bottom = Right,
+	};
+
+private:
+	std::wstring m_Name;
+	Component* m_Component = nullptr;
+	std::vector<Division> m_Children;
+
+	std::optional<Length> m_LeftMargin, m_RightMargin, m_TopMargin, m_BottomMargin;
+	std::optional<Length> m_LeftPadding, m_RightPadding, m_TopPadding, m_BottomPadding;
+	Length m_Width, m_Height;
+
+	Alignment m_HorizontalAlignment = Left, m_VerticalAlignment = Top;
+	double m_EvaluatedX = 0, m_EvaluatedY = 0;
+
+public:
+	Division(std::wstring name, Length width, Length height) noexcept;
+	Division(Component* component, Length width, Length height) noexcept;
+	Division(std::wstring name, Component* component, Length width, Length height) noexcept;
+	Division(const Division&) = delete;
+	Division(Division&& division) noexcept = default;
+	~Division() = default;
+
+public:
+	Division& operator=(const Division&) = delete;
+	Division& operator=(Division&& division) noexcept = default;
+
+public:
+	void AddChild(Division&& child);
+
+	Division& SetMargin(const Length& margin);
+	Division& SetHorizontalMargin(const Length& horizontalMargin);
+	Division& SetVerticalMargin(const Length& verticalMargin);
+	Division& SetLeftMargin(Length leftMargin) noexcept;
+	Division& SetRightMargin(Length rightMargin) noexcept;
+	Division& SetTopMargin(Length topMargin) noexcept;
+	Division& SetBottomMargin(Length bottomMargin) noexcept;
+	Division& SetPadding(const Length& padding);
+	Division& SetHorizontalPadding(const Length& horizontalPadding);
+	Division& SetVerticalPadding(const Length& verticalPadding);
+	Division& SetLeftPadding(Length leftPadding) noexcept;
+	Division& SetRightPadding(Length rightPadding) noexcept;
+	Division& SetTopPadding(Length topPadding) noexcept;
+	Division& SetBottomPadding(Length bottomPadding) noexcept;
+
+	Division& SetHorizontalAlignment(Alignment horizontalAlignment) noexcept;
+	Division& SetVerticalAlignment(Alignment verticalAlignment) noexcept;
 };
